5d86eaa0f24843a86a059eaa34de49346f221fba
Elliot <elliotgould5721@gmail.com>
2024-08-24 18:10:14 +0100

    Solved the error of tailwind not working

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./index.css";
+import Nav from './Nav';
+import Hero from './hero'
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <>
+    {/* <Hero /> */}
+    </>
+    </div>
+    
+  )
+  
+}
+
+export default App;
